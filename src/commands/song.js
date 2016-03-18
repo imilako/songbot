dafe59eaca79506be4fccb80308454873326501f
imilako <inoven@gmail.com>
2016-03-18 23:31:27 +0100

    Use join() instead of all()

--- src/commands/song.js
+++ src/commands/song.js
@@ -11,6 +11,7 @@ export default class Song {
                     'song name pls',
                     'what song is this',
                     'what\'s the song?',
+                    'what is this song',
                     'what song this is',
                     'name of this song?'  ];
     chatMonitor.registerCommand(this, this.song, '', alias);
@@ -18,20 +19,16 @@ export default class Song {
 
     this.bot = bot;
     this.cooldown = 60;
+    this.maxCheckLength = 300000;
     this.lastUsed = moment().subtract(this.cooldown, 's');
-  }
 
-  song (user, args, isPhrase) {
-    if (moment().diff(this.lastUsed, 'seconds') < this.cooldown) return false;
-    this.lastUsed = moment();
-    let username = user['display-name'] || user.username;
-
-    let hypem = {
+    this.hypem = {
       uri: `https://api.hypem.com/v2/users/${creds.hypemUser}/history?key=swagger`,
       method: 'GET',
       json: true
     }
-    let lastfm = {
+
+    this.lastfm = {
       uri: 'http://ws.audioscrobbler.com/2.0',
       method: 'GET',
       json: true,
@@ -43,40 +40,41 @@ export default class Song {
         user: `${creds.lastfUser}`
       }
     }
-    let promises = [];
-    promises.push(request(hypem));
-    promises.push(request(lastfm));
+  }
 
-    Promise.all(promises)
-      .then(p => {
-        let songHypem = p[0][0];
-        let songLastfm = p[1].recenttracks.track[0];
+  song (user, args, isPhrase) {
+    if (moment().diff(this.lastUsed, 'seconds') < this.cooldown) return false;
+    this.lastUsed = moment();
+    let username = user['display-name'] || user.username;
 
-        let elapsedHypem = moment().diff(moment.unix(songHypem.ts_played));
-        let nowScrobbling = songLastfm['@attr']
-          ? true
-          : false;
-        let elapsedLastfm = nowScrobbling
-          ? false
-          : moment().diff(moment.unix(songLastfm.date.uts));
+    Promise.join(request(this.hypem), request(this.lastfm), (songHypem, rawLastFm) => {
+      let songLastfm = rawLastFm.recenttracks.track[0];
 
-        let song = '';
-        if (elapsedHypem < 300000)
-          song = `${songHypem.artist} - ${songHypem.title}`;
-        else if (nowScrobbling)
-          song = `${songLastfm.artist['#text']} - ${songLastfm.name}`;
-        else if (elapsedLastfm < 300000)
-          song = `${songLastfm.artist['#text']} - ${songLastfm.name}`;
-        else
-          return console.log('--> No song detected in the last 5 min!');
+      let elapsedHypem = moment().diff(moment.unix(songHypem.ts_played));
+      let nowScrobbling = songLastfm['@attr']
+        ? true
+        : false;
+      let elapsedLastfm = nowScrobbling
+        ? false
+        : moment().diff(moment.unix(songLastfm.date.uts));
 
-        /* only whisper when phrase detected
-        let sent = isPhrase
-          ? false
-          : this.bot.say(song); */
-        let sent = this.bot.say(song);
-        if (!sent) this.bot.whisper(username, song);
-        console.log(`--> ${song}`);
-      })
+      let song = '';
+      if (elapsedHypem < this.maxCheckLength)
+        song = `${songHypem.artist} - ${songHypem.title}`;
+      else if (nowScrobbling)
+        song = `${songLastfm.artist['#text']} - ${songLastfm.name}`;
+      else if (elapsedLastfm < this.maxCheckLength)
+        song = `${songLastfm.artist['#text']} - ${songLastfm.name}`;
+      else
+        return console.log('--> No song detected in the last 5 min!');
+
+      /* only whisper when phrase detected
+      let sent = isPhrase
+        ? false
+        : this.bot.say(song); */
+      let sent = this.bot.say(song);
+      if (!sent) this.bot.whisper(username, song);
+      console.log(`--> ${song}`);
+    })
   }
 }
